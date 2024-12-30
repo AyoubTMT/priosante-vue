fa3c52f46539f70b7c24477bb11b43c7000e38b5
MOHAMED AYOUB TAJMOUT <m.tajmout@eca-assurances.com>
2024-12-30 17:20:42 +0100

    nouveau logo

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,7 +4,7 @@
       <nav class="navbar-container container">
         <a href="/" class="home-link">
           <div class="navbar-logo">
-            <router-link :to="{ name: 'home' }"><img className='' src='../assets/images/logo.png' style="width: 250px; height: 97px;"
+            <router-link :to="{ name: 'home' }"><img className='' src='../assets/images/AssurLogo.png' style="width: 250px; height: 93.8px;"
                 alt="" /></router-link>
           </div>
         </a>
